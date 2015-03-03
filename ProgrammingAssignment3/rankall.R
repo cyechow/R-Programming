50878897c198c210025477b036828650a9db64b1
Edith <edith.cy.chow@gmail.com>
2015-03-03 01:00:56 -0500

    Modified rankall.R - removed use of a variable.

--- ProgrammingAssignment3/rankall.R
+++ ProgrammingAssignment3/rankall.R
@@ -23,36 +23,30 @@ rankall <- function(outcome, num = "best") {
   
   ## For each state, find the hospital of the given rank
   counter <- 0 ## Setting up row counter
-  stateCol <- data$State
-  states <- sort(as.character(unique(stateCol)))
+  ## Get all unique states:
+  states <- sort(as.character(unique(data$State)))
   for (state in states) {
-    stateDeathRates <- deathRates[stateCol == state]
-    hospitals <- data$Hospital.Name[stateCol == state]
+    stateDeathRates <- deathRates[data$State == state]
+    hospitals <- data$Hospital.Name[data$State == state]
     
     ## Get order permutation for lowest to highest, remove all NA:
     perm <- order(stateDeathRates, hospitals, na.last = NA, decreasing = FALSE)
     
-    if (num == "best") {
-      N <- 1
-    }
-    else if (num == "worst") {
-      N <- length(perm)
-    }
-    else if (!is.numeric(num)) {
-      N <- 0
-    }
-    else {
-      N <- num
-    }
+    ## Note: num cannot change, made that mistake initially. Set N based on "num" input.
+    if (num == "best") { N <- 1 }
+    else if (num == "worst") { N <- length(perm) }
+    else if (!is.numeric(num)) { N <- 0 }
+    else { N <- num }
     
     if (length(stateDeathRates) < N || length(stateDeathRates) == 0 || N <= 0) {
-      newRow <- c("<NA>", state)
+      newRow <- c("<NA>", state) ## "<NA>" is based on the assignment requirement, could easily be something else.
     }
     else {
       newRow <- c(hospitals[perm[N]], state)
     }
     
     rankedHospitals[seq(counter + 1, nrow(rankedHospitals) + 1), ] <- newRow
+    ## This sets the row name to be the state. There might be an easier way to do it, but it works.
     row.names(rankedHospitals)[counter + 1] <- state
     counter <- counter + 1
   }
